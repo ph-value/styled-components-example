67b1b75cf71d00337eceffe9c7385c61295ff780
ph-value <phvalue123@gmail.com>
2018-10-28 00:00:51 +0900

    injectGlobal and Extend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+createGlobalStyle`
+    body{
+        padding:0;
+        margin:0;
+    }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Button>Hello</Button>
+        <Button danger>Hello</Button>
+        <Anchor href="https://google.com">Go to Google</Anchor>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%
+  background-color: #7ed6df;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearence: none;
+  cursor: pointer;
+  &active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#eb4d4b" : "#badc58")};
+`;
+
+
+const Anchor = styled(Button.withComponent('a'))`
+    text-decoration: none;
+`;
+
+
+
 
 export default App;
