292114a213e5da49946290f415689f10c5ec17bd
ph-value <phvalue123@gmail.com>
2018-10-30 14:49:44 +0900

    Extra Attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 createGlobalStyle`
     body{
@@ -8,17 +8,12 @@ createGlobalStyle`
     }
 `;
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Button>Hello</Button>
-        <Button danger>Hello</Button>
-        <Anchor href="https://google.com">Go to Google</Anchor>
-      </Container>
-    );
-  }
-}
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
 
 const Container = styled.div`
   height: 100vh;
@@ -26,37 +21,19 @@ const Container = styled.div`
   background-color: #7ed6df;
 `;
 
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearence: none;
-  cursor: pointer;
-  &active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? "#eb4d4b" : "#badc58")};
-  ${props => {
-      if(props.danger){
-        return css`animation: ${rotation} 2s linear infinite;`;
-      }
-  }}
+const Input = styled.input.attrs({ required: true })`
+  border: none;
+  ${awesomeCard}
 `;
 
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from{
-      transform: rotate(0deg);
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Input placeholder="hello" />
+      </Container>
+    );
   }
-  to{
-    transform: rotate(360deg);
-  }
-`;
+}
 
 export default App;
